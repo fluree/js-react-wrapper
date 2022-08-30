8d3d7a62729d4b744177897717f45c291d2b8836
Daniel Petranek <dpetranek@flur.ee>
2022-08-30 15:10:04 -0500

    Fix useFlureeQuery import

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import FlureeProvider from './FlureeProvider';
 import FlureeConn from './FlureeConn';
-import useFlureeQuery from './FlureeQuery';
+import useFlureeQuery from './flureeQuery';
 import useForceTime from './useForceTime';
 
 export { FlureeConn, FlureeProvider, useFlureeQuery, useForceTime };
-
